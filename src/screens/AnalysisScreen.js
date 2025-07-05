d21c338dfcf76c7a999206a698285782682e3c98
ThanhBinh4811 <binhntph50571@gmail.com>
2025-07-05 09:51:54 +0700

    Temporary edit on AnalysisScreen for testing

--- src/screens/AnalysisScreen.js
+++ src/screens/AnalysisScreen.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
-const AnalysisScreen = () => {
+const AnalysisScreen = ({ navigation }) => {
   return (
     <View>
       <Text>AnalysisScreen</Text>
+      <TouchableOpacity
+        style={{ marginTop: 220, width: 40, height: 40, backgroundColor: '#3BB3FD', alignItems: 'center', justifyContent: 'center', borderRadius: 8 }}
+        onPress={() => navigation.navigate('WaterEntryScreen')}
+      >
+        <Text style={{ color: '#fff' }}>Go</Text>
+      </TouchableOpacity>
     </View>
   )
 }
