e68d3ec94ef122fd635ed33122b2257586e8c09b
DatNtPH51025 <datntph51025@gmail.com>
2025-07-14 01:25:55 +0700

    fix/homeScreen

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://192.168.1.8:3000/api';
-export const API_SONG_URL = 'http://192.168.1.8:3000'
+export const API_BASE_URL = 'http://192.168.1.16:3000/api';
+export const API_SONG_URL = 'http://192.168.1.16:3000'
 
-export const AUTH_URL     = `${API_BASE_URL}/auth`;
-export const SONG_URL     = `${API_SONG_URL}/songs/song`;
+export const AUTH_URL = `${API_BASE_URL}/auth`;
+export const SONG_URL = `${API_SONG_URL}/songs/song`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- src/screens/ExerciseScreen/service/exerciseService.js
+++ src/screens/ExerciseScreen/service/exerciseService.js
@@ -1,7 +1,20 @@
-import axios from 'axios';
-import { API_BASE_URL } from '../../../redux/config';
+import axios from "axios";
+import { API_BASE_URL } from "../../../redux/config";
 
-export const fetchExercisesFromBackend = async () => {
-    const res = await axios.get(`${API_BASE_URL}/exercises`);
-    return res.data;
+
+export const fetchExercisesFromBackend = async (token = null) => {
+    try {
+        const res = await axios.get(`${API_BASE_URL}/exercises`, {
+            headers: token ? { Authorization: `Bearer ${token}` } : {},
+        });
+
+        console.log("📦 Dữ liệu API:", res.data);
+
+        return Array.isArray(res.data)
+            ? res.data
+            : res.data.data || [];
+    } catch (err) {
+        console.error("❌ Lỗi API exercises:", err.response?.data || err.message);
+        return [];
+    }
 };
